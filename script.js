e080afaab614af211e4d52c64d0c407960a4bf6d
Ash Weston <ash-w@live.com.au>
2022-01-19 15:28:05 +1000

    update to Brisbane as default

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
   var APIKey = "82ad17d25281f665f0ef2bd44088ca51";
 
+  $(".time").text("hello");
+  $(".date").text(m.format("dddd MMMM DD YYYY")); //set current day
+
+  //Set the default city to Brisbane//
   getWeatherData();
 
   function getWeatherData() {
-    navigator.geolocation.getCurrentPosition((success) => {
-      let { latitude, longitude } = success.coords;
-
-      fetch(
-        `https:api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${APIKey}`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          showWeatherData(data);
-          console.log(data);
-        });
+    var url = `https:api.openweathermap.org/data/2.5/weather?q=Brisbane&units=metric&appid=${APIKey}`;
+    $.get(url, function (data, status) {
+      console.log(data);
     });
   }
 
-  function showWeatherData(data) {}
+  //Display the default city//
+  showWeatherData();
+  function showWeatherData(data) {
+    $("#timezone").text("");
+    $("#country").text("");
+  }
 });
